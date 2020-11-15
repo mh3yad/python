4287ab987c7f3e9a032d5006009a87a95539f6ec
mohamed <mhayad010@gmail.com>
2020-11-16 00:56:26 +0200

    Create simple_oop_banking_sys.py

---
+++ simple_oop_banking_sys/simple_oop_banking_sys.py
@@ -0,0 +1,26 @@
+class User:
+    def __init__(self,name,age,total):
+        self.name  = name
+        self.age = age
+        self.total = total
+    def show(self):
+        print("username: "+self.name + " age is " + int(self.age))   
+class Bank(User):
+    def __init__(self,name,age,total):
+        super().__init__(name,age,total)
+        self.amount = None
+    def deposit(self,amount):
+        self.total +=amount
+        
+        print("account balance has been updated $ " +  str(self.total))
+    def withdrow(self,amount):
+        if self.total < amount:
+            print("insuffient amount total is $ " + str(self.total))
+            return
+        self.total -= amount
+        return self.total
+    def show(self):
+        return self.age
+bank = Bank("ayad",15,500)
+bank.deposit(12)
+bank.withdrow(800)    
